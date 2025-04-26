7b7afa65d394d72e075f62682174b3cbea29d627
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-04-26 19:17:43 +0545

    added servlet to manage bookings

---
+++ src/main/java/com/example/aerodoot/controller/ManageBookingsServlet.java
@@ -0,0 +1,14 @@
+package com.example.aerodoot.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ManageBookingsServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/view/manage-bookings.jsp").forward(request, response);
+    }
+}
