acd77d898c8c0cd378783a4d565e9c43598a352e
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-04-26 19:26:38 +0545

    provides a appropriate values in @web servlet to access jsp from servlet

--- src/main/java/com/example/aerodoot/controller/AdminAnalyticsServlet.java
+++ src/main/java/com/example/aerodoot/controller/AdminAnalyticsServlet.java
@@ -1,14 +1,16 @@
 package com.example.aerodoot.controller;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet("/admin/analytics")
 public class AdminAnalyticsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/view/admin/analysis.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/view/admin/analytics.jsp").forward(request, response);
     }
 }
--- src/main/java/com/example/aerodoot/controller/SettingsServlet.java
+++ src/main/java/com/example/aerodoot/controller/SettingsServlet.java
@@ -1,12 +1,14 @@
 package com.example.aerodoot.controller;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet("/admin/manage-setting")
 public class SettingsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/view/admin/setting.jsp").forward(request, response);
