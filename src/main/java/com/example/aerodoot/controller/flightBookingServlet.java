87c14d8cced5fb300bddfc44ef08079bb1f4def8
munal777 <munalpoudel3@gmail.com>
2025-05-04 01:09:43 +0545

    starting the servlet testing of flightBokking

--- src/main/java/com/example/aerodoot/controller/flightBookingServlet.java
+++ src/main/java/com/example/aerodoot/controller/flightBookingServlet.java
@@ -12,6 +12,22 @@ import java.io.IOException;
 public class flightBookingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("get method of flight-booking");
+        request.getRequestDispatcher("/WEB-INF/view/flightBooking.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String departureLocation = request.getParameter("departure");
+        String arrivalLocation = request.getParameter("destination");
+        String departureDate = request.getParameter("departureDate");
+        String returnDate = request.getParameter("returnDate");
+        String passenger = request.getParameter("passenger");
+
+        System.out.println(departureDate + " -> " + departureLocation + " " + returnDate + " -> " + arrivalLocation + " " + passenger);
+
         request.getRequestDispatcher("/WEB-INF/view/flightBooking.jsp").forward(request, response);
     }
 }
+
+
