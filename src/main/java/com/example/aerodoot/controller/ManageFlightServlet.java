a455447da3fc1a852662beb7a24b6eab5a3974cd
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-05-04 01:46:07 +0545

    created init method for flights to get all the flights

--- src/main/java/com/example/aerodoot/controller/ManageFlightServlet.java
+++ src/main/java/com/example/aerodoot/controller/ManageFlightServlet.java
@@ -1,5 +1,7 @@
 package com.example.aerodoot.controller;
 
+import com.example.aerodoot.dao.FlightDAO;
+import com.example.aerodoot.model.Flight;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,9 +9,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/admin/manage-flight")
 public class ManageFlightServlet extends HttpServlet {
+    private List<Flight> flights;
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("FlightInitServlet: Initialization started...");
+        try {
+            flights = FlightDAO.getAllFlights();
+            getServletContext().setAttribute("flights", flights);
+            System.out.println("Flights loaded successfully.");
+        } catch (Exception e) {
+            throw new ServletException("Flight initialization failed", e);
+        }
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/view/admin/manage-flights.jsp").forward(request, response);
     }
